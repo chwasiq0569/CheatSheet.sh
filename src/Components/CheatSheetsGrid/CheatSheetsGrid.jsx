ff24ee1799a4f76a64df8ba6f33ddcbad64e9cdf
wasiq abdullah <chwasiq0569>
2023-08-24 00:36:36 +0500

    Created Grid Component

---
+++ src/Components/CheatSheetsGrid/CheatSheetsGrid.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const CheatSheetsGrid = () => {
+    return (<div style={{ border: "2px solid blue" }} class="flex flex-wrap ">
+        <div style={{ width: "250px", margin: "0 3rem", height: '250px', border: "2px solid red" }}>01</div>
+        <div style={{ width: "250px", margin: "0 3rem", height: '250px', border: "2px solid red" }}>01</div>
+        <div style={{ width: "250px", margin: "0 3rem", height: '250px', border: "2px solid red" }}>01</div>
+        <div style={{ width: "250px", margin: "0 3rem", height: '250px', border: "2px solid red" }}>01</div>
+        <div style={{ width: "250px", margin: "0 3rem", height: '250px', border: "2px solid red" }}>01</div>
+        <div style={{ width: "250px", margin: "0 3rem", height: '250px', border: "2px solid red" }}>01</div>
+        <div style={{ width: "250px", margin: "0 3rem", height: '250px', border: "2px solid red" }}>01</div>
+    </div>);
+}
+
+export default CheatSheetsGrid;
--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -1,19 +1,12 @@
 import React from 'react'
+import CheatSheetsGrid from '../../Components/CheatSheetsGrid/CheatSheetsGrid';
 import Hero from '../../Components/Hero/Hero';
 import Navbar from '../../Components/Navbar/Navbar';
 const HomePage = () => {
     return (<div className='min-h-screen gradient-bg'>
         <Navbar />
         <Hero />
-        <div class="flex flex-wrap">
-            <div style={{ width: "250px", margin: "0 4rem", height: '250px', border: "2px solid red" }}>01</div>
-            <div style={{ width: "250px", margin: "0 4rem", height: '250px', border: "2px solid red" }}>01</div>
-            <div style={{ width: "250px", margin: "0 4rem", height: '250px', border: "2px solid red" }}>01</div>
-            <div style={{ width: "250px", margin: "0 4rem", height: '250px', border: "2px solid red" }}>01</div>
-            <div style={{ width: "250px", margin: "0 4rem", height: '250px', border: "2px solid red" }}>01</div>
-            <div style={{ width: "250px", margin: "0 4rem", height: '250px', border: "2px solid red" }}>01</div>
-            <div style={{ width: "250px", margin: "0 4rem", height: '250px', border: "2px solid red" }}>01</div>
-        </div>
+        <CheatSheetsGrid />
     </div>);
 }
 
